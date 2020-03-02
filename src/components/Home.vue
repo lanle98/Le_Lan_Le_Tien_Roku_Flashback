e97a825dcc35df0aece99051eaac3a68b30ebc4d
Darren Le <darrenle98@gmail.com>
2020-03-02 00:07:21 -0500

    added css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Header />
+    <Nav />
     <Container :musics="musics" :tvshows="tvshows" :movies="movies" />
   </div>
 </template>
--- src/components/home_components/Container.vue
+++ src/components/home_components/Container.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="container">
-    <section class="popular">
-      <h2>Popular</h2>
-    </section>
-
-    <section class="movies-section row m-5">
-      <h2 class="col-12 text-left">Movies</h2>
-      <div class="movies col-4" :key="movie.id" v-for="movie in movies">
+    <section class="popular justify-content-center row m-5">
+      <h2 class="col-12 text-left">Popular</h2>
+      <div class="movies col-md-4 col-12 p-0" :key="movie.id" v-for="movie in movies.slice(0,3)">
         <img class="img-fluid p-2" :src="'images/'+movie.cover" />
       </div>
     </section>
 
-    <section class="movies-section row m-5">
+    <section class="movies-section justify-content-center row m-5">
+      <h2 class="col-12 text-left">Movies</h2>
+      <div class="movies col-md-2 col-12 p-0" :key="movie.id" v-for="movie in movies.slice(0,5)">
+        <img class="img-fluid p-2" :src="'images/'+movie.cover" />
+      </div>
+    </section>
+
+    <section class="movies-section justify-content-center row m-5">
       <h2 class="col-12 text-left">TV Shows</h2>
-      <div class="movies col-4" :key="tvshow.id" v-for="tvshow in tvshows">
+      <div class="movies col-md-2 col-12 p-0" :key="tvshow.id" v-for="tvshow in tvshows.slice(0,5)">
         <img class="img-fluid p-2" :src="'images/'+tvshow.cover" />
       </div>
     </section>
 
-    <section class="movies-section row m-5">
+    <section class="movies-section justify-content-center row m-5">
       <h2 class="col-12 text-left">Musics</h2>
-      <div class="movies col-4" :key="music.id" v-for="music in musics">
+      <div class="movies col-md-2 col-12 p-0" :key="music.id" v-for="music in musics.slice(0,5)">
         <img class="img-fluid p-2" :src="'images/'+music.cover" />
       </div>
     </section>
