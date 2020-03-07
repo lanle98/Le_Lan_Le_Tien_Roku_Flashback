4bc3f1260e9fc75904e5806a539ab229aa83c570
Darren Le <darrenle98@gmail.com>
2020-03-07 15:27:03 -0500

    minor changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'))
 app.get(/.*/, (req, res) => [
-    res.sendFile(__dirname + '/dist/index.html')
+    res.sendfile(__dirname + '/dist/index.html')
 ])
 
 app.listen(port);
