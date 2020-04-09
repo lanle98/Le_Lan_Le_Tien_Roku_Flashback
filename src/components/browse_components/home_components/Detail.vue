3d47e2b54025b6f6afacefec502a68ddac7bbb92
Darren Le <darrenle98@gmail.com>
2020-04-09 17:30:09 -0400

    moved videos folder

--- src/components/browse_components/home_components/Detail.vue
+++ src/components/browse_components/home_components/Detail.vue
@@ -4,6 +4,9 @@
       <div class="modal-content p-5">
         <div v-if="this.$props.detail">
           <img class="pb-5" :src="'images/'+ detail.cover" />
+          <video width="320" height="240" controls>
+            <source :src="'videos/'+detail.link" type="video/mp4" />
+          </video>
           <h2>{{detail.title}}</h2>
           <p>{{detail.desc}}</p>
         </div>
